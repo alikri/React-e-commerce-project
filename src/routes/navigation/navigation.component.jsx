291d111fabe4bd561c7593185a2def7a56ded9e0
Ali Kri <alinanosovets6849@gmail.com>
2022-09-02 13:12:52 +0200

    end of lesson 118, secrtion 9

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,7 +14,7 @@ const Navigation = () => { //it's a TOP level component now!!!
 	const { currentUser } = useContext(UserContext);
 	const { isCartOpen } = useContext(CartContext);
 
-	
+
 	// console.log(currentUser);
 
 	// const signOutHandler = async () => {
@@ -44,8 +44,9 @@ const Navigation = () => { //it's a TOP level component now!!!
 					}
 					<CartIcon />
 				</div>
-				{isCartOpen && <CartDropdown />} {/* it will try to evaluate the thruthiness of both sides. On the rigth side is a component.
-				 Component is always a true value. So, if both are true, it will return the last value, which is a component in this case. 
+				{isCartOpen && <CartDropdown />}
+				{/* it will try to evaluate the thruthiness of both sides. On the rigth side is a component.
+				 Component is always a true value. So, if both are true, it will return the last value, which is a component in this case.
 				 If the first value is false, it won't return anything. So, here basically if both are true - the dropdown will show up */}
 
 			</div>
