6ee74e882d97d0386cf08ac7025eb78bc25bda2b
Ali Kri <alinanosovets6849@gmail.com>
2022-09-13 13:24:28 +0200

    end of lesson 137, styled via styled-components button component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,16 +1,30 @@
-import "./button.styles.scss";
+import {BaseButton, GoogleSignInButton, InvertedButton, DropdownButton } from  "./button.styles";
 
-const BUTTON_TYPE_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
+	base: "base",
 	google: "google-sign-in",
 	inverted: "inverted",
 	dropdown: "dropdown"
 }
 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
+	{ 
+		[BUTTON_TYPE_CLASSES.base]: BaseButton,
+		[BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+		[BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+		[BUTTON_TYPE_CLASSES.dropdown]: DropdownButton,
+		
+	}[buttonType]
+)
 
-const Button = ({ children, buttonType, ...otherProps}) => {
-	return <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+
+	const CustomButton = getButton(buttonType);
+
+	return <CustomButton
 	{...otherProps}
-	> {children} </button >
+	> {children} </CustomButton >
 };
 
 export default Button;
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"; 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import "./sign-in-form.styles.scss";
 
 // import { UserContext } from "../../contexts/user.context"; we centrilizing this feature via useEffect() in user.context.jsx
@@ -81,7 +81,7 @@ const SignInForm = () => {
 				
 				<div className="buttons-container">
 					<Button type="submit" >Sign In</Button>
-					<Button type="button" buttonType="google" onClick={signInWithGoogle} >Google Sign In</Button>
+					<Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle} >Google Sign In</Button>
 				</div>
 				
 			</form>
